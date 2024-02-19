f30b92e562dc110412582621837d02dbafee5e4b
AdrianoMendes <adriano.alyson@gmail.com>
2024-02-19 01:25:08 -0300

    mapa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-//import Mapa from './components/mapa'
+import Mapa from './components/mapa'
 import Drawer from './components/drawer'
 
 function App() {
   return (
     <>
       <Drawer />
+      <Mapa />
     </>
   )
 }
