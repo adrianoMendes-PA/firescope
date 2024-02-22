6f87963652521f6c4347aab2c6105218ab482cdd
AdrianoMendes <adriano.alyson@gmail.com>
2024-02-22 01:26:25 -0300

    localização do usuário

--- src/components/mapa/index.jsx
+++ src/components/mapa/index.jsx
@@ -1,26 +1,45 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Map, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import RoomIcon from '@mui/icons-material/Room';
 
 function App() {
   const accessToken = import.meta.env.VITE_TOKEN_MAP_BOX;
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    const watchID = navigator.geolocation.watchPosition(handlePositionReceived);
+    return () => navigator.geolocation.clearWatch(watchID);
+  }, []);
+
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords;
+    setLocation({ latitude, longitude });
+  }
 
   return (
-    <Map
-      mapboxAccessToken={accessToken}
-      initialViewState={{
-        longitude: -47.9113191,
-        latitude: -1.2972993,
-        zoom: 12
-      }}
-      style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
-      mapStyle="mapbox://styles/mapbox/streets-v12"
-    >
-      <Marker longitude={-47.9113191} latitude={-1.2972993} anchor="bottom">
-        <RoomIcon fontSize='large' color='primary'/>
-      </Marker>
-    </Map>
+    <>
+      {location.latitude && location.longitude ? (
+        <Map
+          mapboxAccessToken={accessToken}
+          initialViewState={{
+            longitude: location.longitude,
+            latitude: location.latitude,
+            zoom: 12
+          }}
+          style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
+          mapStyle="mapbox://styles/mapbox/streets-v12"
+        >
+          <Marker longitude={location.longitude} latitude={location.latitude} anchor="bottom">
+            <RoomIcon fontSize='large' color='primary' />
+          </Marker>
+        </Map>
+      ) : (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <p>Carregando mapa...</p>
+        </div>
+      )}
+    </>
   );
 }
 
