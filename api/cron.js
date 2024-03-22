faa84cacdefb10642e5e9f0223a558653f7cd40f
AdrianoMendes <adriano.alyson@gmail.com>
2024-03-22 14:13:28 -0300

    fix: vercel.json nova hora - teste

--- api/cron.js
+++ api/cron.js
@@ -4,8 +4,10 @@ async function main() {
     try {
         await downloadAndConvertCSV();
         console.log("CSV downloaded and converted successfully!");
+        res.status(200).end('CSV downloaded and converted successfully!');
     } catch (error) {
         console.error('Error downloading or converting CSV:', error);
+        res.status(500).end('Internal Server Error');
     }
 }
 
