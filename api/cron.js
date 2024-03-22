bcf261502dcb400baea2a08bb3ccbb5477fc0492
AdrianoMendes <adriano.alyson@gmail.com>
2024-03-22 13:34:40 -0300

    nova hora definida para o cron

--- api/cron.js
+++ api/cron.js
@@ -3,6 +3,7 @@ import { downloadAndConvertCSV } from './baixarCSVQueimadas.js';
 export default async function handler(req, res) {
     try {
         await downloadAndConvertCSV();
+        console.log("CSV downloaded and converted successfully!");
         res.status(200).end('CSV downloaded and converted successfully!');
     } catch (error) {
         console.error('Error downloading or converting CSV:', error);
