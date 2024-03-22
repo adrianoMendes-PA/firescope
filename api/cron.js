221541a6589a14b5b4e1e0e6e8927310bcc6625c
AdrianoMendes <adriano.alyson@gmail.com>
2024-03-22 13:59:05 -0300

    fix: vercel.json e no cron

--- api/cron.js
+++ api/cron.js
@@ -1,12 +1,13 @@
 import downloadAndConvertCSV from './baixarCSVQueimadas.js';
 
-export default async function handler(req, res) {
+async function main() {
     try {
         await downloadAndConvertCSV();
         console.log("CSV downloaded and converted successfully!");
-        res.status(200).end('CSV downloaded and converted successfully!');
     } catch (error) {
         console.error('Error downloading or converting CSV:', error);
-        res.status(500).end('Internal Server Error');
     }
 }
+
+// Chama a função principal
+main();
