39c250f01eee660cc9de9137e914c583b7616ecb
Adriano Mendes <adriano.alyson@gmail.com>
2024-08-20 20:30:58 -0300

    update: barChart

--- baixarCSVQueimadas.js
+++ baixarCSVQueimadas.js
@@ -82,4 +82,4 @@ async function convertCSVtoJSON(csvFilePath, tmpPath) {
 }
 
 // Comentar quando for subir
-//downloadAndConvertCSV();
+downloadAndConvertCSV();
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,25 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Grid, Card, CardContent, Typography } from '@mui/material';
+import { BarChart } from '@mui/x-charts/BarChart';
 
 const Dashboard = () => {
   const [estadoComMaisFocos, setEstadoComMaisFocos] = useState('');
   const [municipioComMaisFocos, setMunicipioComMaisFocos] = useState('');
   const [biomaDoMunicipio, setBiomaDoMunicipio] = useState('');
+  const [totalFocos, setTotalFocos] = useState('');
 
   useEffect(() => {
-    
     async function fetchData() {
       try {
         const response = await fetch('/latest.json');
         const data = await response.json();
 
-        // Inicializando variáveis para armazenar o estado, município e bioma com mais focos de queimada
         let maxFocos = 0;
         let estadoEncontrado = "";
         let municipioEncontrado = "";
         let biomaEncontrado = "";
 
-        // Iterando sobre os registros no array
         data.forEach(registro => {
           const focosQueimada = parseFloat(registro.frp) || 0;
 
@@ -31,10 +30,10 @@ const Dashboard = () => {
           }
         });
 
-        // Atualizando os estados
         setEstadoComMaisFocos(estadoEncontrado);
         setMunicipioComMaisFocos(municipioEncontrado);
         setBiomaDoMunicipio(biomaEncontrado);
+        setTotalFocos(maxFocos);
       } catch (error) {
         console.error("Erro ao buscar dados:", error);
       }
@@ -46,7 +45,7 @@ const Dashboard = () => {
   return (
     <Container>
       <Grid container spacing={2}>
-        <Grid item xs={6} md={4}>
+        <Grid item xs={6} md={3}>
           <Card>
             <CardContent>
               <Typography sx={{ fontSize: 25 }} color="text.secondary" gutterBottom>
@@ -58,7 +57,7 @@ const Dashboard = () => {
             </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={6} md={4}>
+        <Grid item xs={6} md={3}>
           <Card>
             <CardContent>
               <Typography sx={{ fontSize: 25 }} color="text.secondary" gutterBottom>
@@ -70,7 +69,7 @@ const Dashboard = () => {
             </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={6} md={4}>
+        <Grid item xs={6} md={3}>
           <Card>
             <CardContent>
               <Typography sx={{ fontSize: 25 }} color="text.secondary" gutterBottom>
@@ -81,6 +80,40 @@ const Dashboard = () => {
               </Typography>
             </CardContent>
           </Card>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 25 }} color="text.secondary" gutterBottom>
+                Total de focos
+              </Typography>
+              <Typography variant="body2">
+                {totalFocos}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} mt={4}>
+        <Grid item xs={12}>
+          <BarChart
+            xAxis={[
+              {
+                id: 'barCategories',
+                data: ['Estado', 'Município', 'Bioma', 'Total de focos'],
+                scaleType: 'band',
+              },
+            ]}
+            series={[
+              {
+                data: [1, 1, 1, totalFocos],
+                color: ['#1976d2', '#1976d2', '#1976d2', '#d32f2f']
+              },
+            ]}
+            width={500}
+            height={300}
+          />
         </Grid>
       </Grid>
     </Container>
