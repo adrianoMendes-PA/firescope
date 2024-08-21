69edfadc3a330c81f3c3f08ad55771dc48ccf6dd
Adriano Mendes <adriano.alyson@gmail.com>
2024-08-20 22:49:41 -0300

    update: novo card

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grid, Card, CardContent, Typography } from '@mui/material';
+import { Container, Grid, Card, CardContent, Typography, List, ListItem, ListItemText } from '@mui/material';
 import { BarChart } from '@mui/x-charts/BarChart';
 
 const Dashboard = () => {
   const [estadoComMaisFocos, setEstadoComMaisFocos] = useState('');
   const [municipioComMaisFocos, setMunicipioComMaisFocos] = useState('');
   const [biomaDoMunicipio, setBiomaDoMunicipio] = useState('');
-  const [totalFocos, setTotalFocos] = useState('');
+  const [totalFocos, setTotalFocos] = useState(0);
+  const [dadosChart, setDadosChart] = useState([0, 0, 0, 0]);
 
   useEffect(() => {
     async function fetchData() {
@@ -14,13 +15,21 @@ const Dashboard = () => {
         const response = await fetch('/latest.json');
         const data = await response.json();
 
+        let totalFocosEstado = 0;
+        let totalFocosMunicipio = 0;
+        let totalFocosBioma = 0;
         let maxFocos = 0;
-        let estadoEncontrado = "";
-        let municipioEncontrado = "";
-        let biomaEncontrado = "";
+        let estadoEncontrado = '';
+        let municipioEncontrado = '';
+        let biomaEncontrado = '';
+
+        // Inicializando um mapa para contar os focos de cada categoria
+        let focosPorEstado = {};
+        let focosPorMunicipio = {};
+        let focosPorBioma = {};
 
         data.forEach(registro => {
-          const focosQueimada = parseFloat(registro.frp) || 0;
+          const focosQueimada = parseFloat(registro.frp) / 1000 || 0;
 
           if (focosQueimada > maxFocos) {
             maxFocos = focosQueimada;
@@ -28,12 +37,39 @@ const Dashboard = () => {
             municipioEncontrado = registro.municipio;
             biomaEncontrado = registro.bioma;
           }
+
+          // Contagem de focos por estado
+          if (focosPorEstado[registro.estado]) {
+            focosPorEstado[registro.estado] += focosQueimada;
+          } else {
+            focosPorEstado[registro.estado] = focosQueimada;
+          }
+
+          // Contagem de focos por município
+          if (focosPorMunicipio[registro.municipio]) {
+            focosPorMunicipio[registro.municipio] += focosQueimada;
+          } else {
+            focosPorMunicipio[registro.municipio] = focosQueimada;
+          }
+
+          // Contagem de focos por bioma
+          // if (focosPorBioma[registro.bioma]) {
+          //   focosPorBioma[registro.bioma] += focosQueimada;
+          // } else {
+          //   focosPorBioma[registro.bioma] = focosQueimada;
+          // }
         });
+
+        totalFocosEstado = focosPorEstado[estadoEncontrado] || 0;
+        totalFocosMunicipio = focosPorMunicipio[municipioEncontrado] || 0;
+        totalFocosBioma = focosPorBioma[biomaEncontrado] || 0;
 
         setEstadoComMaisFocos(estadoEncontrado);
         setMunicipioComMaisFocos(municipioEncontrado);
         setBiomaDoMunicipio(biomaEncontrado);
         setTotalFocos(maxFocos);
+
+        setDadosChart([totalFocosEstado, totalFocosMunicipio, maxFocos]);
       } catch (error) {
         console.error("Erro ao buscar dados:", error);
       }
@@ -88,7 +124,7 @@ const Dashboard = () => {
                 Total de focos
               </Typography>
               <Typography variant="body2">
-                {totalFocos}
+                {totalFocos.toFixed(3)}
               </Typography>
             </CardContent>
           </Card>
@@ -96,24 +132,33 @@ const Dashboard = () => {
       </Grid>
 
       <Grid container spacing={2} mt={4}>
-        <Grid item xs={12}>
+        <Grid item xs={12} md={8}>
           <BarChart
             xAxis={[
               {
                 id: 'barCategories',
-                data: ['Estado', 'Município', 'Bioma', 'Total de focos'],
+                data: [estadoComMaisFocos, municipioComMaisFocos, 'Total de focos'],
                 scaleType: 'band',
               },
             ]}
             series={[
               {
-                data: [1, 1, 1, totalFocos],
-                color: ['#1976d2', '#1976d2', '#1976d2', '#d32f2f']
+                data: dadosChart,
               },
             ]}
-            width={500}
-            height={300}
+            width={800}
+            height={400}
+            margin={{ top: 20, bottom: 50, left: 60, right: 50 }}
           />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 25 }} color="text.secondary" gutterBottom>
+                Satélites
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </Container>
